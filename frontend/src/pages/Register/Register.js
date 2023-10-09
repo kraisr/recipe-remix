4562f587280d78b6b0d5e7f9760d9f29ed8d86de
kraisr <27452581+kraisr@users.noreply.github.com>
2023-10-09 00:31:17 -0400

    add logo to register and login

--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -2,10 +2,12 @@ import * as React from "react";
 import "./register.css";
 import Box from "@mui/material/Box";
 import RegisterForm from "./RegisterForm";
+import Logo from "../../components/Navbar/Logo";
 
 function Register() {
   return (
     <Box>
+      <Logo />
       <Box
         // width={isNonMobileScreens ? "50%" : "93%"}
         p="2rem"
