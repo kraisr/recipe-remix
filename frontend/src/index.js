9b51832255203908ae4cd97f29f304016d3949ea
Tim Chou <tim.c.whitewolf@gmail.com>
2023-10-10 16:04:53 -0400

    fix light dark mode globalization problem

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import authReducer from "./state";
@@ -16,32 +16,78 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { PersistGate } from "redux-persist/integration/react"
+import { PersistGate } from "redux-persist/integration/react";
 
-//import 'bootstrap/dist/css/bootstrap.min.css';
+// Create a functional component for your application
+function Root() {
+  useEffect(() => {
+    const fetchUserSettings = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          throw new Error("No token found");
+        }
 
-const persistConfig = { key: "root", storage, version: 1 };
-const persistedReducer = persistReducer(persistConfig, authReducer);
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Ignore these action types
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+        const response = await fetch("http://localhost:8080/user/user", {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+          },
+          method: "GET",
+        });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
-);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
 
+        const data = await response.json();
 
+        if (data.mode) {
+          const rootElement = document.getElementById("root");
+
+          if (!rootElement.classList.contains("dark-mode")) {
+            rootElement.classList.remove("light-mode");
+            rootElement.classList.add("dark-mode");
+          }
+        } else {
+          const rootElement = document.getElementById("root");
+
+          if (!rootElement.classList.contains("light-mode")) {
+            rootElement.classList.remove("dark-mode");
+            rootElement.classList.add("light-mode");
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user settings:", error);
+      }
+    };
+
+    fetchUserSettings();
+  }, []);
+
+  const persistConfig = { key: "root", storage, version: 1 };
+  const persistedReducer = persistReducer(persistConfig, authReducer);
+  const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          // Ignore these action types
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  });
+
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistStore(store)}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+// Render the Root component to the root element
+ReactDOM.render(<Root />, document.getElementById("root"));
