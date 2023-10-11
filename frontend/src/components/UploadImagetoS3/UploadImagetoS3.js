36a3a1056c0aa94928d9b0cc8ea6053af59d7ab1
kraisr <27452581+kraisr@users.noreply.github.com>
2023-10-11 02:07:44 -0400

    add error message to google sign in

--- frontend/src/components/UploadImagetoS3/UploadImagetoS3.js
+++ frontend/src/components/UploadImagetoS3/UploadImagetoS3.js
@@ -1,35 +1,35 @@
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 
-const uploadImageToS3 = async (image, bucketName, key) => {
+// const uploadImageToS3 = async (image, bucketName, key) => {
     
-  // Initialize S3 client
-  const s3 = new AWS.S3({
-    region: 'us-east-1', // Replace with your AWS region
-    accessKeyId: 'AKIAXGUOMVWBK7RQWCPT', // Replace with your AWS access key
-    secretAccessKey: 'pAkcyl5MDFIp32j+ZkKMefUDAwIWs1JSnFlYppS3', // Replace with your AWS secret key
-  });
+//   // Initialize S3 client
+//   const s3 = new AWS.S3({
+//     region: 'us-east-1', // Replace with your AWS region
+//     accessKeyId: 'AKIAXGUOMVWBK7RQWCPT', // Replace with your AWS access key
+//     secretAccessKey: 'pAkcyl5MDFIp32j+ZkKMefUDAwIWs1JSnFlYppS3', // Replace with your AWS secret key
+//   });
 
-  const imageBlob = new Blob([image]);
+//   const imageBlob = new Blob([image]);
 
-  // Prepare the parameters for the S3 upload
-  const params = {
-    Bucket: bucketName,
-    Key: key,
-    Body: imageBlob,
-    ContentType: image.type,
-    ACL: 'public-read', // Make the uploaded image publicly accessible
-  };
+//   // Prepare the parameters for the S3 upload
+//   const params = {
+//     Bucket: bucketName,
+//     Key: key,
+//     Body: imageBlob,
+//     ContentType: image.type,
+//     ACL: 'public-read', // Make the uploaded image publicly accessible
+//   };
 
-  try {
-    // Upload the image to S3
-    const uploadResult = await s3.upload(params).promise();
+//   try {
+//     // Upload the image to S3
+//     const uploadResult = await s3.upload(params).promise();
 
-    // Return the URL of the uploaded image
-    return uploadResult.Location;
-  } catch (error) {
-    console.error('Error uploading image to S3:', error);
-    throw error; // Re-throw the error for handling at a higher level
-  }
-};
+//     // Return the URL of the uploaded image
+//     return uploadResult.Location;
+//   } catch (error) {
+//     console.error('Error uploading image to S3:', error);
+//     throw error; // Re-throw the error for handling at a higher level
+//   }
+// };
 
-export default uploadImageToS3;
+// export default uploadImageToS3;
