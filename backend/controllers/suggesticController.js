fdc295fc47e65165fef68c06b9b20c4dc4e8a298
Ryan Leonard <rleonard772@gmail.com>
2023-10-19 18:50:00 -0400

    recipe schema

--- backend/controllers/suggesticController.js
+++ backend/controllers/suggesticController.js
@@ -47,5 +47,53 @@ export const searchIngredients = async (req, res) => {
     }
 };
 
+export const searchRecipes = async (req, res) => {
+    console.log("searchRecipes endpoint hit");
+    try {
+        
+        const ingredientList = req.body;
+        console.log(ingredientList);
+        const query = `
+        {
+            searchRecipesByIngredients(
+              mustIngredients: ${ingredientList}
+            ) {
+              edges {
+                node {
+                  name
+                  ingredients {
+                    name
+                  }
+                 ingredientLines
+                }
+              }
+            }
+          }
+        `;
+// databaseId
+//id
+
+        const response = await fetch("https://production.suggestic.com/graphql", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${process.env.SUGGESTIC_TOKEN}`,
+                "sg-user": process.env.SUGGESTIC_USER_ID
+            },
+            body: JSON.stringify({ query })
+        });
+
+        const data = await response.json();
+
+        console.log("Data from Suggestic API:", data);
+
+        res.json(data);
+
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error", details: error.message });
+    }
+};
+
 
 
---
+++ backend/models/Recipe.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema ({
+    id: {
+
+    },
+    ingredients: [{
+        ingredientName: {
+            type: String,
+            required:true,
+        }
+    }],
+    instructions: {
+        type: String,
+        required: true,
+    }, 
+
+    image: {
+        type: String,
+        required:true,
+    }
+
+})
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
