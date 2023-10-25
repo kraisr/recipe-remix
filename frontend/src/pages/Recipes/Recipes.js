0e9cfd463e17b01feb5a102555b3f53d857e8661
michael01890 <limichael640@gmail.com>
2023-10-24 21:46:52 -0400

    Create recipes page

---
+++ frontend/src/pages/Recipes/Recipes.js
@@ -0,0 +1,11 @@
+import "./recipes.css";
+import React, { useEffect, useState } from "react";
+
+const Recipes = () => {
+    return (
+        <div>Recipes</div>
+
+    )
+}
+
+export default Recipes;
