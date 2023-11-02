2abd5cc05b84e723933a9180db40890d886415e4
kraisr <27452581+kraisr@users.noreply.github.com>
2023-11-02 02:19:55 -0400

    viewing post

---
+++ frontend/src/components/Post/Post.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import "./post.css";
+
+
+const Post = ({ recipe }) => {
+
+  return (
+    <div className="recipe-content">
+
+      <div className="name-container">
+        <h1>
+          {recipe.name} 
+        </h1>
+      </div>
+
+      <div className="image-container">
+        <img src={recipe.image} alt={recipe.name} className="recipe-image" />
+      </div>
+
+      <div className="time-container">
+        <div>
+            <span style={{ fontWeight: 'bold', marginRight: '0.5em' }}>Total Time:</span>
+            {recipe.totalTime}
+        </div>
+      </div>
+
+      <div>
+          <span style={{ fontWeight: 'bold', marginRight: '0.5em' }}>Number of Servings:</span>
+          {recipe.numberOfServings}
+      </div>
+
+      <div className="ingredientLines-container">
+        <div className="window-title">Ingredients:</div>
+        <ul>
+            {/* {recipe.ingredients.map((line, index) => (
+                <li key={index}>{line}</li>
+            ))} */}
+        </ul>
+      </div>
+
+      <div className="instructions-container">
+        <div className="window-title">Instructions:</div>
+        {/* <ol className="instructions-list">
+            {recipe.instructions.map((instr, index) => (
+                <li key={index} className="instruction-line">{instr}</li>
+            ))}
+        </ol> */}
+      </div>
+  </div>
+  );
+};
+
+
+export default Post;
--- frontend/src/pages/Community/Community.js
+++ frontend/src/pages/Community/Community.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./community.css";
 import CreatePost from '../../components/CreatePost/CreatePost.js';
+import Post from '../../components/Post/Post.js';
 
 const Community = () => {
     const [isPostWindowOpen, setIsPostWindowOpen] = useState(false);
-    
-    
     const [recipes, setRecipes] = useState([]);
-
+    const [posts, setPosts] = useState([]);
+    const [clickedRecipe, setClickedRecipe] = useState('');
+    const [currentPost, setCurrentPost] = useState(null);
 
     const fetchUserPosts = async () => {
         try {
@@ -34,12 +35,29 @@ const Community = () => {
                 // Update the recipes state with the user's posts
                 const recipeNames = data.posts.map(post => post.name);
                 setRecipes(recipeNames);
+                setPosts(data.posts);
             } else {
                 console.error('Invalid data format:', data);
             }
         } catch (error) {
             console.error('Error fetching user posts:', error);
         }
+    };
+
+    const test = () => {
+        console.log("recipes: ", recipes);
+        console.log("posts: ", posts);
+    }
+
+    const handlePostClick = (recipeName) => {
+      // Find the recipe object
+      const foundRecipe = posts.find(post => post.name === recipeName);
+      console.log("found recipe: ", foundRecipe)
+      console.log("posts: ", posts);
+      console.log("recipeName: ", recipeName);
+      // Set the states of clickedRecipe and currentPost
+      setClickedRecipe(recipeName);
+      setCurrentPost(foundRecipe);
     };
 
     // Fetch user's posts when the component mounts
@@ -51,15 +69,19 @@ const Community = () => {
         <div className="community-container">
             <div className="left-panel">
                 <div className="posted-title">
-                    <h4>My Posted Recipes</h4>  
+                    <h4>My Posted Recipes</h4>
                 </div>
                 <hr />
                 
                 <div className="recipe-grid">
                     {recipes.map((recipe, index) => (
-                        <div key={index} className="recipe-item button-33">
-                            {recipe}
-                        </div>
+                      <ul className="recipe-list">
+                        <li>
+                          <div key={index} className="recipe-item button-33" onClick={() => handlePostClick(recipe)}>
+                              {recipe}
+                          </div>
+                        </li>
+                      </ul>
                     ))}
                 </div>
 
@@ -67,14 +89,20 @@ const Community = () => {
                     <i className="fas fa-plus"></i>
                 </button>
             </div>
+            
+            {/* <button onClick={test}>Create Post</button> */}
 
-            <div className="center-panel">
-
+            <div className="scroll-wrapper">
+              <div className="center-panel">
+                {currentPost && (
+                  <Post recipe={currentPost} />
+                )}
+              </div>
             </div>
 
-            <div className="right-panel">
+            {/* <div className="right-panel">
 
-            </div>
+            </div> */}
 
             {isPostWindowOpen && (
                 <CreatePost
