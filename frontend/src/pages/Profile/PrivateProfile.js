6748b7ee6f808e6665d879aa6627112aeff26aa0
kraisr <27452581+kraisr@users.noreply.github.com>
2023-11-14 02:37:48 -0500

    more styling fixes for profile lmao

--- frontend/src/pages/Profile/PrivateProfile.js
+++ frontend/src/pages/Profile/PrivateProfile.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import VisibilityOffIcon from '@material-ui/icons/VisibilityOff'; // Importing the icon
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import './privateProfile.css';
 
 const PrivateProfile = () => {
   return (
     <div className="private-profile">
-      <VisibilityOffIcon style={{ fontSize: 40 }} />
+      <VisibilityOffIcon style={{ fontSize: '10rem' }} />
       <p className="private-profile-text">Private Profile</p>
     </div>
   );
