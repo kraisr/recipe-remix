75dfcea8da756a6d666208f3e2e3f9c553aeb2ea
kraisr <27452581+kraisr@users.noreply.github.com>
2023-11-14 13:37:34 -0500

    state management for categories + styling done

--- frontend/src/pages/Community/Community.js
+++ frontend/src/pages/Community/Community.js
@@ -30,6 +30,7 @@ const Community = () => {
     const [searchResults, setSearchResults] = useState({ users: [], posts: [] });
     const navigate = useNavigate();
     const cuisines = ["Italian", "Mexican", "Japanese", "Mediterranean", "Indian", "Party Food"];
+    const [selectedCuisines, setSelectedCuisines] = useState([]);
     const [userPosts, setUserPosts] = useState([]);
 
     const handleSearch = async (searchTerm) => {
@@ -117,138 +118,154 @@ const Community = () => {
       }
   };
 
-    const handlePostClick = (postId) => {
-      // Find the recipe object
+  const handlePostClick = (postId) => {
+    // Find the recipe object
+    setCurrentPostId(postId);
+  };
+  useEffect(() => {
+    if (postId) {
       setCurrentPostId(postId);
-    };
-    useEffect(() => {
-      if (postId) {
-        setCurrentPostId(postId);
+    }
+  }, [postId]);
+  
+
+  // Fetch user's posts when the component mounts
+  useEffect(() => {
+      fetchAllPosts();
+      fetchUserPosts();
+  }, []);
+
+  const handleCuisineClick = (cuisine) => {
+    setSelectedCuisines(prevSelected => {
+      if (prevSelected.includes(cuisine)) {
+        return prevSelected.filter(item => item !== cuisine);
+      } else {
+        return [...prevSelected, cuisine];
       }
-    }, [postId]);
-    
+    });
+    console.log("selected cuisines: ", selectedCuisines);
+  };
 
-    // Fetch user's posts when the component mounts
-    useEffect(() => {
-        fetchAllPosts();
-        fetchUserPosts();
-    }, []);
 
-    return (
-        <div className="community-container">
-            <div className="left-panel">
-                <center>
-                    <button className="create-post-btn button-44" onClick={() => setIsPostWindowOpen(true)}>
-                      <i className="fas fa-plus"></i>
-                    </button>
-                </center>
-
-                <div className="posted-title">
-                    <h5>My Posted Recipes</h5>
-                </div>
-                {/* <hr /> */}
-                
-                <div className="recipe-grid">
-                  {userPosts.map((post) => (
-                    <ul className="recipe-list" key={post._id}>
-                      <li>
-                        <div className="recipe-item button-33" onClick={() => handlePostClick(post._id)}>
-                            {post.name}
-                        </div>
-                      </li>
-                    </ul>
-                  ))}
-                </div>
-
-                <div className="divide-line"></div>
-                
-                <div className="posted-title">
-                    <h5>Categories</h5>
-                </div>
-                <div className="categories-grid">
-                    {cuisines.map((cuisine, index) => (
-                        <div key={index} className="category-item">
-                            {cuisine}
-                        </div>
-                    ))}
-                </div>
-            </div>
-
-            <div className="center-panel">
+  return (
+      <div className="community-container">
+          <div className="left-panel">
               <center>
-              <div className="search-input-container">
-                <input 
-                    type="text" 
-                    placeholder="Search Posts or Users..." 
-                    value={searchTerm} 
-                    onChange={(e) => handleSearch(e.target.value)} 
-                    className="search-input"
-                />
-                  <div className="search-suggestions-container">
-                  <ul className="search-suggestions-dropdown">
-                    {searchResults.users.map((user, index) => (
-                      <li key={`user-${index}`} className="suggestion-item" onClick={() => navigate(`/profile/${user.token}`)}>
-                        {user.firstName} {user.lastName} ({user.username})
-                      </li>
-                    ))}
-                    {searchResults.users.length > 0 && searchResults.posts.length > 0 && (
-                      <li className="divider">------ POSTS ------</li>
-                    )}
-                    {searchResults.posts.map((post, index) => (
-                      <li key={`post-${index}`} className="suggestion-item">
-                        {post.name} - {post.caption}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                </div>
+                  <button className="create-post-btn button-44" onClick={() => setIsPostWindowOpen(true)}>
+                    <i className="fas fa-plus"></i>
+                  </button>
               </center>
-              <div className="center-button">
-                <button className="community-button">Ratings</button>
-                <button className="community-button">Recentness</button>
+
+              <div className="posted-title">
+                  <h5>My Posted Recipes</h5>
+              </div>
+              {/* <hr /> */}
+              
+              <div className="recipe-grid">
+                {userPosts.map((post) => (
+                  <ul className="recipe-list" key={post._id}>
+                    <li>
+                      <div className="recipe-item button-33" onClick={() => handlePostClick(post._id)}>
+                          {post.name}
+                      </div>
+                    </li>
+                  </ul>
+                ))}
               </div>
 
-              <div className="c-post-grid">
-                  {posts.map((post) => (
-                    <ul className="post-list" key={post._id}>
-                      <li>
-                        <div className="post-item" onClick={() => handlePostClick(post._id)}>
-                            <h4>{post.name}</h4>
-                            <div className="subtitle">
-                              <div className="author"> 
-                                <p>{post.user.firstName} {post.user.lastName}</p> 
-                              </div>
-                              <div className="time">
-                                <p>{post.timeAgo}</p>
-                              </div>
-                            </div>
-                            
-                        </div>
-                      </li>
-                    </ul>
+              <div className="divide-line"></div>
+              
+              <div className="posted-title">
+                  <h5>Categories</h5>
+              </div>
+              <div className="categories-grid">
+                  {cuisines.map((cuisine, index) => (
+                      <div 
+                        key={index} 
+                        className={`category-item ${selectedCuisines.includes(cuisine) ? 'active' : ''}`}
+                        onClick={() => handleCuisineClick(cuisine)}
+                      >
+                          {cuisine}
+                      </div>
                   ))}
               </div>
+          </div>
 
-            </div>
-
-            <div className="scroll-wrapper">
-              <div className="right-panel">
-                {currentPostId && (
-                  <Post postId={currentPostId} />
-                )}
+          <div className="center-panel">
+            <center>
+            <div className="search-input-container">
+              <input 
+                  type="text" 
+                  placeholder="Search Posts or Users..." 
+                  value={searchTerm} 
+                  onChange={(e) => handleSearch(e.target.value)} 
+                  className="search-input"
+              />
+                <div className="search-suggestions-container">
+                <ul className="search-suggestions-dropdown">
+                  {searchResults.users.map((user, index) => (
+                    <li key={`user-${index}`} className="suggestion-item" onClick={() => navigate(`/profile/${user.token}`)}>
+                      {user.firstName} {user.lastName} ({user.username})
+                    </li>
+                  ))}
+                  {searchResults.users.length > 0 && searchResults.posts.length > 0 && (
+                    <li className="divider">------ POSTS ------</li>
+                  )}
+                  {searchResults.posts.map((post, index) => (
+                    <li key={`post-${index}`} className="suggestion-item">
+                      {post.name} - {post.caption}
+                    </li>
+                  ))}
+                </ul>
               </div>
+              </div>
+            </center>
+            <div className="center-button">
+              <button className="community-button">Ratings</button>
+              <button className="community-button">Recentness</button>
             </div>
 
-            {isPostWindowOpen && (
-              <ThemeProvider theme={theme}>
-                <CreatePost
-                    isOpen={isPostWindowOpen}
-                    onRequestClose={() => setIsPostWindowOpen(false)}
-                />
-              </ThemeProvider>
-            )}
-        </div>
-    );
+            <div className="c-post-grid">
+                {posts.map((post) => (
+                  <ul className="post-list" key={post._id}>
+                    <li>
+                      <div className="post-item" onClick={() => handlePostClick(post._id)}>
+                          <h4>{post.name}</h4>
+                          <div className="subtitle">
+                            <div className="author"> 
+                              <p>{post.user.firstName} {post.user.lastName}</p> 
+                            </div>
+                            <div className="time">
+                              <p>{post.timeAgo}</p>
+                            </div>
+                          </div>
+                          
+                      </div>
+                    </li>
+                  </ul>
+                ))}
+            </div>
+
+          </div>
+
+          <div className="scroll-wrapper">
+            <div className="right-panel">
+              {currentPostId && (
+                <Post postId={currentPostId} />
+              )}
+            </div>
+          </div>
+
+          {isPostWindowOpen && (
+            <ThemeProvider theme={theme}>
+              <CreatePost
+                  isOpen={isPostWindowOpen}
+                  onRequestClose={() => setIsPostWindowOpen(false)}
+              />
+            </ThemeProvider>
+          )}
+      </div>
+  );
 }
 
 export default Community;
